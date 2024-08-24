a72c99a33052a1d22ccc1e5eb66b64273cf2acfe
Nithishkumar R (MS/ESG1-VM) <R.Nithishkumar@in.bosch.com>
2024-08-24 20:53:12 +0530

    Test case added

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -12,11 +12,29 @@ extern PrinterForControllerPtr_t printerForControllerPtr;
 
 void checkAndAlert( AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
 
-TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) 
+TEST(TypeWiseAlertTestSuite,PassiveCoolingForController) 
 {
     printerForControllerPtr = PrinterForControllerMOCK;
     BatteryCharacter Test1 = {PASSIVE_COOLING,"BrandA"};
     checkAndAlert(TO_CONTROLLER,Test1,40);
     EXPECT_EQ(header_Tester,0xfeed);
     EXPECT_EQ(breachType_Tester,2);
+}
+
+TEST(TypeWiseAlertTestSuite,HiActiveCoolingForController) 
+{
+    printerForControllerPtr = PrinterForControllerMOCK;
+    BatteryCharacter Test1 = {HI_ACTIVE_COOLING,"BrandA"};
+    checkAndAlert(TO_CONTROLLER,Test1,-1);
+    EXPECT_EQ(header_Tester,0xfeed);
+    EXPECT_EQ(breachType_Tester,0);
+}
+
+TEST(TypeWiseAlertTestSuite,MedActiveCoolingForController) 
+{
+    printerForControllerPtr = PrinterForControllerMOCK;
+    BatteryCharacter Test1 = {MED_ACTIVE_COOLING,"BrandA"};
+    checkAndAlert(TO_CONTROLLER,Test1,1);
+    EXPECT_EQ(header_Tester,0xfeed);
+    EXPECT_EQ(breachType_Tester,1);
 }
